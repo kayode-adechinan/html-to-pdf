69e9c0729b6e6dd33e7ed95a6f55f5813b144fb8
Kayode Adechinan <kayode.adechinan@gmail.com>
2023-08-24 15:29:23 +0000

    updates

--- tests/example.pdf.spec.ts
+++ tests/example.pdf.spec.ts
@@ -1,10 +1,10 @@
 import { test, expect } from "@playwright/test";
 
 test("has title", async ({ page }) => {
-  await page.goto(
-    "https://www.bannerbear.com/blog/how-to-download-images-from-a-website-using-puppeteer/"
-  );
-  await page.pdf({ path: `result.pdf` });
+  // await page.goto(
+  //   "https://www.bannerbear.com/blog/how-to-download-images-from-a-website-using-puppeteer/"
+  // );
+  // await page.pdf({ path: `result.pdf` });
 
   // Expect a title "to contain" a substring.
   await expect(1).toBe(1);
